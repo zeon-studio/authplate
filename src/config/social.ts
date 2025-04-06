7bfb5cdb79121f555e136f7d58f4b6260db29580
Somrat <58769763+tfsomrat@users.noreply.github.com>
2025-04-06 15:26:02 +0600

    update theme.css and social icons

---
+++ src/config/social.ts
@@ -0,0 +1,21 @@
+import { SiFacebook, SiGithub, SiX } from "@icons-pack/react-simple-icons";
+
+const Social = [
+  {
+    name: "facebook",
+    icon: SiFacebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    name: "twitter",
+    icon: SiX,
+    link: "https://twitter.com/",
+  },
+  {
+    name: "github",
+    icon: SiGithub,
+    link: "https://www.github.com/",
+  },
+];
+
+export default Social;
