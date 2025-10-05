ebaa536936878ff6396be9f41c14847f19225f27
Somrat <58769763+tfsomrat@users.noreply.github.com>
2025-10-05 16:31:25 +0600

    fixed navigation dropdown

--- src/layouts/partials/Header/NavigationMenu.tsx
+++ src/layouts/partials/Header/NavigationMenu.tsx
@@ -32,13 +32,21 @@ export function NavigationMenu({
         <NavItem key={i} variant={menu.hasChildren ? "dropdown" : "default"}>
           {menu.hasChildren && menu.children?.length ? (
             <>
-              <span className={`nav-link inline-flex items-center`}>
+              <input
+                type="checkbox"
+                id={`submenu-${menu.name}`}
+                className="peer hidden lg:hidden"
+              />
+              <label
+                htmlFor={`submenu-${menu.name}`}
+                className={`nav-link inline-flex items-center`}
+              >
                 {menu.name}
                 <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
                   <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
                 </svg>
-              </span>
-              <ul className="nav-dropdown-list hidden group-hover:block lg:invisible lg:absolute lg:block lg:opacity-0 lg:group-hover:visible lg:group-hover:opacity-100">
+              </label>
+              <ul className="nav-dropdown-list hidden peer-checked:block lg:invisible lg:absolute lg:block lg:opacity-0 lg:group-hover:visible lg:group-hover:opacity-100">
                 {menu.children?.map((child, i) => (
                   <NavItem variant="dropdown" key={i}>
                     <NavLink
